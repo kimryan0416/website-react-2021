f0d2229d07bfb16e03bf4d98e884f20fbe357551
Ryan Kim <kimryan0416@gmail.com>
2021-08-13 09:27:42 -0400

    Updated tucan fitness page with images and further descriptions of development process

--- src/screens/Portfolio/Projects/TucanFitness/TucanFitness.js
+++ src/screens/Portfolio/Projects/TucanFitness/TucanFitness.js
@@ -3,7 +3,13 @@ import {
 	Button,
 	ExtURL, 
 } from "../../../../components";
-import { thumbnail } from "./assets";
+import { 
+	thumbnail,
+	stage1,
+	stage2,
+	uiScreenshot,
+	createRace,
+} from "./assets";
 
 function TucanFitness() {
 	return (
@@ -37,9 +43,16 @@ function TucanFitness() {
 				<Divider space={16} />
 				<h4><strong>Description</strong></h4>
 				<Divider space={8} />
-				<p>I was largely responsible for helping the Tucan Fitness front-end development team update their existing user interface and introduce UX Design concepts into their development process.</p>
+				<p>
+					<img className="PortfolioContentImage Right" src={uiScreenshot} alt="Screenshot of the Tucan Fitness web app user interface" />
+					I was largely responsible for helping the Tucan Fitness front-end development team update their existing user interface and introduce UX Design concepts into their development process.
+				</p>
 				<Divider space={8} />
 				<p>While on this project, I spearheaded the design process by teaching TF’s core staff and developers about how to integrate user interview findings into key feature sets. I also acted as the platform’s primary front-end developer, working closely together with the team’s back-end engineers to add new functions and update outdated processes.</p>
+				<Divider space={16} />
+				<h4><strong>About Tucan Fitness</strong></h4>
+				<Divider space={8} />
+				<p>Tucan Fitness was a startup venture led by graduates of Cornell University and Cornell Tech that aimed to allow people to participate in "virtual races". Participants would track the distance they ran while exercising in private, upload that data either manually or automatically through <ExtURL href="https://www.strava.com/">Strava</ExtURL> to Tucan Fitness's servers, and avatars on a virtual map would proceed along the virtual route. In doing so, racers could still exercise in private while participating in competitive racing.</p>
 				<Divider space={16} />
 				<h4><strong>Quick Details:</strong></h4>
 				<Divider space={8} />
@@ -77,6 +90,46 @@ function TucanFitness() {
 					</li>
 				</ul>
 				<Divider space={16} />
+				<h4><strong>Development Process</strong></h4>
+				<Divider space={8} />
+				<p>Progress in Tucan Fitness during my work period was split into two major stages. The first stage required me to improve the user interface of the Tucan Fitness web application, whereas the second required me to fully create, test, and launch a new collection of screens and functions that would allow normal users to create virtual races of their own.</p>
+				<Divider space={8} />
+				<div className="PortfolioContentColumns">
+					<div className="PortfolioContentColumn">
+						<h6><strong>Stage 1: UI Updates and Optimization</strong></h6>
+						<Divider space={16} />
+ 						<img className="PortfolioContentImage" src={stage1} alt="Stage 1" />
+					</div>
+					<div className="ShowInMobile"><Divider space={16} /></div>
+					<div className="PortfolioContentColumn">
+						<h6><strong>Stage 2 2: "Create Race" Feature &amp; Documentation</strong></h6>
+						<Divider space={16} />
+						<img className="PortfolioContentImage" src={stage2} alt="Stage 2" />
+					</div>
+				</div>
+				<Divider space={8} />
+				<p>Crucial to Stage 1 was the introduction of UX Design concepts that would solve a crucial issue: the lack of direction regarding which features to incorporate into the web app. This conundrum came from two distinct issues:</p>
+				<Divider space={8} />
+				<ol>
+					<li>Disagreements on how the company should proceed in its design pattern and what features to add.</li>
+					<li>A lack of knowledge on how current users navigated the app and what features they wanted.</li>
+				</ol>
+				<Divider space={8} />
+				<p>The introduction of UX Design processes, from user interviews and brand guideline definitions to empathy maps and personas, solved both of these issues simultaneously. Brand Guidelines, agreed upon by the TF team, centralized which aspects of the UI and functionality that the TF web app would prioritize, while interviews, empathy maps, and personas identified key needs and wants that our target audiences wished to see in the web app.</p>
+				<Divider space={8} />
+				<p>By continously iterating on these interviews and personas within a span of several weeks, similar to an Agile UX process, we were able to update the web app's visual identity and functionality in a way that satisfied a majority of our users.</p>
+				<Divider space={8} />
+				<p>
+					<img className="PortfolioContentImage Right" src={createRace} alt="Screenshot of the Tucan Fitness race creation interface" />
+					The insights gained from users, particularly race directors, from Stage 1 also fueled many of the functionalities we added in Stage 2. Our foresight in gathering user interviews from both racers and race directories enabled us to move quickly in code development, for we already had the personas and empathy maps needed to determine which features to add to the "Create Race" part of the web app.
+				</p>
+				<Divider space={16} />
+				<h4><strong>UX Design Resources:</strong></h4>
+				<Divider space={8} />
+				<p>The experience necessitated the use of templates and processes commonly found in UX Design. An aggregate collection of these templates can be found at the link below for anybody interested in getting started with UX Design:</p>
+				<Divider space={8} />
+				<ExtURL href="https://drive.google.com/drive/folders/1ASUIEqFgnIcWpw2DQM48Okq1c-1PLm8x?usp=sharing"><Button>UX Design Resources</Button></ExtURL>
+				<Divider space={16} />
 				<h4><strong>Lessons Learned:</strong></h4>
 				<Divider space={8} />
 				<p>While I had provided consultation and development services in the past, this was the first time my consultation extended beyond front-end coding and into UX Design practices. This meant I had to employ different tactics to understand the needs of TF’s core team and guide them towards a development process geared towards UX-oriented practices.</p>
@@ -104,12 +157,6 @@ function TucanFitness() {
 						</ul>
 					</div>
 				</div>
-				<Divider space={16} />
-				<h4><strong>Public Resources:</strong></h4>
-				<Divider space={8} />
-				<p>The experience necessitated the use of templates and processes commonly found in UX Design. An aggregate collection of these templates can be found at the link below for anybody interested in getting started with UX Design:</p>
-				<Divider space={8} />
-				<ExtURL href="https://drive.google.com/drive/folders/1ASUIEqFgnIcWpw2DQM48Okq1c-1PLm8x?usp=sharing"><Button>UX Design Resources</Button></ExtURL>
 			</div>
 		</div>
 	);
--- src/screens/Portfolio/Projects/TucanFitness/assets/index.js
+++ src/screens/Portfolio/Projects/TucanFitness/assets/index.js
@@ -1,5 +1,13 @@
 import thumbnail from "./thumbnail.png";
+import stage1 from "./TucanFitness_Stage1.svg";
+import stage2 from "./TucanFitness_Stage2.svg";
+import uiScreenshot from "./TucanFitnessUISmall.png";
+import createRace from "./TucanFitnessCreateRace.png";
 
 export {
 	thumbnail,
+	stage1,
+	stage2,
+	uiScreenshot,
+	createRace,
 }
