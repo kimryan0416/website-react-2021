e4ce9a25170cb83db2d2a4b426a8adc42a3aca14
Ryan Kim <kimryan0416@gmail.com>
2021-08-13 21:57:46 -0400

    Added MIMO to list of work experiences

--- src/screens/Portfolio/Projects/MIMO/MIMO.js
+++ src/screens/Portfolio/Projects/MIMO/MIMO.js
@@ -32,7 +32,9 @@ function MIMO() {
 				<Divider space={8} />
 				<p>I was responsible for the planning, generation, and execution of lessons that taught newcomers to React.js about the library’s key concepts and how to navigate its component-based infrastructure. These lessons are integrated into MIMO’s “React Basics” module, split across eight “Skills” that each represent major aspects of React.js such as state and Functional Components.</p>
 				<Divider space={8} />
-				<p>I was allowed to decide not just what the content was but also how they would build off of one another sequentially across the eight Skills. As “React Basics” was meant for newcomers to React.js, I had to consider how to convey the lesson content in a manner that was easy to understand. The planning phase of the development process took the largest amount of time, whereas the implementation of lesson content into MIMO’s infrastructure took less time as a result of careful planning and adherence to MIMO’s content guidelines.</p>
+				<p>Content for each Skill consists of lessons. Each lesson consists of a description and executable code that students had to complete in order to move to the next lesson. As the lesson planner, I was allowed to decide not just what the content was but also how they would build off of one another sequentially across the eight Skills.</p>
+				<Divider space={8} />
+				<p>Since “React Basics” was meant for newcomers to React.js, I had to consider how to convey the lesson content in a manner that was easy to understand. The planning phase of the development process took the largest amount of time, whereas the implementation of lesson content into MIMO’s infrastructure took less time as a result of careful planning and adherence to MIMO’s content guidelines.</p>
 				<Divider space={16} />
 				<h4><strong>Quick Details:</strong></h4>
 				<Divider space={8} />
@@ -48,7 +50,19 @@ function MIMO() {
 						</ul>
 					</li>
 				</ul>
-			</div>
+				<Divider space={16} />
+				<h4><strong>Project Code</strong></h4>
+				<Divider space={8} />
+				<div className="PortfolioContentColumns">
+					<div className="PortfolioContentColumn">
+ 						<p>Presented here is a public version of the code used inside of the "React Basics" Projects. Students are required to modify each using the concepts taught in the module's Skills. The code samples to the right are the completed versions of each Project.</p>
+					</div>
+					<div className="ShowInMobile"><Divider space={16} /></div>
+					<div className="PortfolioContentColumn">
+						<iframe src="https://kimryan0416.github.io/mimo-projects/" height={460} width={256} />	
+					</div>
+				</div>
+			</div>	
 		</div>
 	);
 }
